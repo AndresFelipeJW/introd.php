05d563978eee1b7c242f84ad4a189233f6273166
AndresFelipeJW <andresjimenez@colegioguanenta.edu.co>
2024-07-29 18:20:10 -0500

    Se agg ejercicio php_3

---
+++ ejercicioPHP_3.php
@@ -0,0 +1,11 @@
+<?php
+// Definir constantes
+define("PI", 3.14159);
+define("GRAVITY", 9.81);
+define("WELCOME_MESSAGE", "¡Bienvenido a nuestra aplicación!");
+
+// Usar constantes
+echo "El valor de PI es: " . PI . "<br>";
+echo "La aceleración debida a la gravedad es: " . GRAVITY . " m/s²<br>";
+echo WELCOME_MESSAGE;
+?>
